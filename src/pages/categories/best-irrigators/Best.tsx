42c2b1f25f28980d043ecc212d7b650f53606200
alejandro yunes <alejandroyunes@outlook.com>
2023-11-27 07:23:39 -0500

    added irrigators comparison

--- src/pages/categories/best-irrigators/Best.tsx
+++ src/pages/categories/best-irrigators/Best.tsx
@@ -6,7 +6,7 @@ import Product from "../../../components/Product/Product"
 import TableOfContent from "../../../components/TableOfContent/TableOfContent"
 import Title from "../../../components/Title/Title"
 import TitleList from "../../../components/TitleList/TitleList"
-import { tableOfContentItems, headerProps, titleProps, comparativeTableProps, titleListProps, firstProduct } from "./data/best"
+import { tableOfContentItems, headerProps, titleProps, comparativeTableProps, titleListProps, firstProduct, secondProduct, thirdProduct, fourthProduct } from "./data/best"
 
 export default function () {
   return (
@@ -23,17 +23,39 @@ export default function () {
       <Header {...headerProps} />
       <TableOfContent items={tableOfContentItems} />
 
-      <Title message={titleProps} />
-
-      <Title id={comparativeTableProps.id} titleH2={comparativeTableProps.title} message={comparativeTableProps.message} />
-      <ComparativeTable items={comparativeTableProps.items} primeUrl={comparativeTableProps.primeUrl} />
       <TitleList {...titleListProps} />
+      <Title message={titleProps} />
 
       {/* First Product */}
       <Title titleH2={firstProduct.proAndCons.title} message={firstProduct.proAndCons.subtitle} />
-      <ProAndCon {...firstProduct.proAndCons} />
       <Product {...firstProduct.product} list={firstProduct.list} />
       <Title {...firstProduct.productDetails} />
+      <ProAndCon {...firstProduct.proAndCons} />
+
+      {/* Second Product */}
+      <Title titleH2={secondProduct.proAndCons.title} message={secondProduct.proAndCons.subtitle} />
+      <Product {...secondProduct.product} list={secondProduct.list} />
+      <Title {...secondProduct.productDetails} />
+      <ProAndCon {...secondProduct.proAndCons} />
+
+      {/* Third Product */}
+
+      <Title titleH2={thirdProduct.proAndCons.title} message={thirdProduct.proAndCons.subtitle} />
+      <Product {...thirdProduct.product} list={thirdProduct.list} />
+      <Title {...thirdProduct.productDetails} />
+      <ProAndCon {...thirdProduct.proAndCons} />
+
+
+      <Title titleH2={fourthProduct.proAndCons.title} message={fourthProduct.proAndCons.subtitle} />
+      <Product {...fourthProduct.product} list={fourthProduct.list} />
+      <Title {...fourthProduct.productDetails} />
+      <ProAndCon {...fourthProduct.proAndCons} />
+
+
+      <Title id={comparativeTableProps.id} titleH2={comparativeTableProps.title} message={comparativeTableProps.message} />
+      <ComparativeTable items={comparativeTableProps.items} primeUrl={comparativeTableProps.primeUrl} />
+
+
 
     </main>
   )
