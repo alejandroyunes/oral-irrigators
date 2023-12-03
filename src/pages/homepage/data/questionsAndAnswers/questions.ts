b78b5f76c01f84b57cb6b7a8543d861e28b60465
alejandro yunes <alejandroyunes@outlook.com>
2023-12-03 12:08:53 -0500

    finished articles

--- src/pages/homepage/data/questionsAndAnswers/questions.ts
+++ src/pages/homepage/data/questionsAndAnswers/questions.ts
@@ -4,12 +4,10 @@ export const questions = {
     {
       question: 'How to use the Dental Irrigator?',
       answer: 'The most important thing is to lean over the sink before starting to shoot the stream of water onto the teeth. We have written a step-by-step User Guide that will assist you with the process.',
-      url: '/how-to-use-the-dental-irrigator'
     },
     {
       question: 'What should I pay attention to when comparing them?',
       answer: "Pay attention to the power, the number of replaceable nozzles, the battery life (if it's a travel model), the water tank capacity, and the reviews from other buyers... Here, you can read about the criteria to consider when purchasing an oral irrigator.",
-      url: '/how-to-use-the-dental-irrigator'
     },
     {
       question: 'How many times a day can the oral irrigator be used?',
@@ -22,12 +20,10 @@ export const questions = {
     {
       question: 'If I have fillings or implants, what type of dental irrigator should I choose?',
       answer: 'Choose one that does not have too much power, as a too powerful stream could weaken the filling. For use with implants, we recommend a model specifically designed for that purpose.',
-      url: '/abs'
     },
     {
       question: 'Is it important for the oral irrigator to have many accessories?',
       answer: "When buying an oral irrigator, don't focus on the number of accessories it comes with, but rather on the use you intend for it. The greater the number of accessories, the higher the price will be.",
-      url: ''
     },
     {
       question: 'Is there any alternative to dental irrigators?',
@@ -36,7 +32,6 @@ export const questions = {
     {
       question: 'What are the risks of using a dental irrigator?',
       answer: 'The main risk is thinking that it is a tool that can be used as the sole cleaning instrument. If we neglect brushing, we will be engaging in improper oral hygiene because dental irrigators should be used as a complement to other cleaning methods. If you are interested in knowing the contraindications it may have, follow the link below.',
-      url: '/123'
     }
   ]
 }
